8b60af12d1e29a386f9061351e8bd26a91c90e56
camilleat <118841268+camilleat@users.noreply.github.com>
2023-04-03 18:28:39 +0200

    Update main2.js

--- main2.js
+++ main2.js
@@ -2,29 +2,29 @@ import {Gpio} from 'onoff';
 
 
 async function main() {
-const pinA = new Gpio(18, "in", "both");
-const pinB = new Gpio(23, "in", "both");
+  const pinA = new Gpio(18, "in", "both");
+  const pinB = new Gpio(23, "in", "both");
 
-let position = 0;
-console.log(`Encoder position: ${position}`);
-const currentState = pinA.read().then((currentState) => {
-  console.log(`Current state of button :${currentState}`);
-              });
+  let position = 0;
+  console.log(`Encoder position: ${position}`);
+  const currentState = pinA.read().then((currentState) => {
+    console.log(`Current state of button :${currentState}`);
+                });
 
-pinA.watch((err, valueA) => {
-  if (err) {
-    throw err;
-  }
-  await pinB.read((err, valueB) => {
+  pinA.watch((err, valueA) => {
     if (err) {
       throw err;
     }
-    if (valueA === valueB) {
-      pos = pos++;
-    } else {
-      pos = pos--;
-    }
-    console.log(`Current position: ${pos}`);
+    pinB.read((err, valueB) => {
+      if (err) {
+        throw err;
+      }
+      if (valueA === valueB) {
+        pos = pos++;
+      } else {
+        pos = pos--;
+      }
+      console.log(`Current position: ${pos}`);
+    });
   });
-});
 }
