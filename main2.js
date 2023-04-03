806c9592d3090c346ec37a4cfebd4c4dadc0a9cf
camilleat <118841268+camilleat@users.noreply.github.com>
2023-04-03 18:10:35 +0200

    Update main2.js

--- main2.js
+++ main2.js
@@ -5,11 +5,22 @@ const pinB = new Gpio(23, "in", "both");
 
 let position = 0;
 console.log(`Encoder position: ${position}`);
-while(true){
 const currentState = pinA.read().then((currentState) => {
   console.log(`Current state of button :${currentState}`);
               });
+
+function handlePinChange(err, value) {
+  if (err) {
+    console.error('Error while reading pin state:', err);
+    return;
+  }
+
+  console.log(`Current state of pin A: ${value}`);
 }
+
+// Listen for changes on the pin and trigger the callback function
+pinA.watch(handlePinChange);
+
 
 pinA.watch((err, valueA) => {
   if (err) {
