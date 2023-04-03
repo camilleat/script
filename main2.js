1ef6036489d32014f4927ccd5ecad9bcac3db3be
Matys53 <119408070+Matys53@users.noreply.github.com>
2023-04-03 18:29:27 +0200

    Update main2.js

--- main2.js
+++ main2.js
@@ -1,30 +1,40 @@
 import {Gpio} from 'onoff';
 
+const pinA = new Gpio(17, 'in', 'both');
+const pinB = new Gpio(18, 'in', 'both');
 
-async function main() {
-  const pinA = new Gpio(18, "in", "both");
-  const pinB = new Gpio(23, "in", "both");
+// Initialisation du compteur
+let counter = 0;
 
-  let position = 0;
-  console.log(`Encoder position: ${position}`);
-  const currentState = pinA.read().then((currentState) => {
-    console.log(`Current state of button :${currentState}`);
-                });
-
-  pinA.watch((err, valueA) => {
-    if (err) {
-      throw err;
+// Fonction pour détecter le mouvement du rotary encoder
+function handleRotation(channel) {
+  if (channel === pinA) {
+    // Si la broche A a changé, déterminer la direction de rotation
+    if (pinA.readSync() !== pinB.readSync()) {
+      counter++;
+    } else {
+      counter--;
     }
-    pinB.read((err, valueB) => {
-      if (err) {
-        throw err;
-      }
-      if (valueA === valueB) {
-        pos = pos++;
-      } else {
-        pos = pos--;
-      }
-      console.log(`Current position: ${pos}`);
-    });
-  });
+  } else {
+    // Si la broche B a changé, déterminer la direction de rotation
+    if (pinA.readSync() === pinB.readSync()) {
+      counter++;
+    } else {
+      counter--;
+    }
+  }
+
+  // Afficher le nouveau compteur
+  console.log('Compteur: ' + counter);
 }
+
+// Ajouter les écouteurs d'événements pour les broches du rotary encoder
+pinA.watch(handleRotation);
+pinB.watch(handleRotation);
+
+// Arrêter la surveillance des broches et nettoyer les ressources lors de la fermeture de l'application
+process.on('SIGINT', function () {
+  pinA.unexport();
+  pinB.unexport();
+  process.exit();
+});
