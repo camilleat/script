e33ab80537e570a505e4f15a2fc6cbdc8ba13c97
Leyo98765 <124131665+Leyo98765@users.noreply.github.com>
2023-04-04 16:51:37 +0200

    Create main3.js
    le code du prof en js

---
+++ main3.js
@@ -0,0 +1,31 @@
+const Gpio = require('onoff').Gpio;
+const clk = new Gpio(17, 'in', 'both');
+const dt = new Gpio(18, 'in', 'both');
+
+let counter = 0;
+let clkLastState = clk.readSync();
+
+clk.watch((err, clkState) => {
+  if (err) {
+    throw err;
+  }
+
+  const dtState = dt.readSync();
+
+  if (clkState !== clkLastState) {
+    if (dtState !== clkState) {
+      counter += 1;
+    } else {
+      counter -= 1;
+    }
+
+    console.log(counter);
+  }
+
+  clkLastState = clkState;
+});
+
+process.on('SIGINT', () => {
+  clk.unexport();
+  dt.unexport();
+});
